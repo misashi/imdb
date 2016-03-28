1ddded31274281883c853585341ecba2c84ae3c5
Misashi <joel.yang.xl@gmail.com>
2016-03-28 14:09:19 +0800

    mistake didn't rebase

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,10 +2,7 @@ class Actor < ActiveRecord::Base
   has_and_belongs_to_many :movies
 
 
-  attr_reader :year_of_birth
-  attr_accessor :age
-
   def age
-    Time.now.year - self.year_of_birth.year
+    Time.now.year - year_of_birth
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :reviews
+  has_many :movies, through: :reviews
+
+  def full_name
+    "#{first_name }  #{last_name}"
+
+  end
 end
