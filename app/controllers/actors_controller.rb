5fc286990087ed8fe8e544ef5ab6f74c5adef3bf
Misashi <joel.yang.xl@gmail.com>
2016-03-28 14:18:47 +0800

    actors and movie controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,52 @@
 class ActorsController < ApplicationController
 
+def index
+    @actors = Actor.all
+end
+
+def new
+  @actor = Actor.new
+end
+
+def create
+  @actor = Actor.new(actor_params)
+
+  @actor.save
+  redirect_to @actor
+end
+
+def show
+  @actor = Actor.find(params[:id])
+end
+
+def edit
+  @actor = Actor.find(params[:id])
+end
+
+def update
+  @actor = Actor.find(params[:id])
+  if @actor.update_attributes(actor_params)
+    redirect_to @actor
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @actor = Actor.find(params[:id]).destroy
+  redirect_to root_path
+end
+
+
+
+  private
+
+  def actor_params
+    params.require(:actor).permit(
+      :name,
+      :year_of_birth,
+      :thumbnail,
+    )
+  end
+
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,37 @@
 class MoviesController < ApplicationController
 
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+
+  def create
+  @movie = Movie.new(movie_params)
+  if @movie.save
+   redirect_to movies_path
+  else
+   render 'new'
+  end
+ end
+
+  def edit
+
+
+  end
+
+  def  show
+    @movie = Movie.find(params[:id])
+  end
+
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :summary, :youtube_embed_id, :thumbnail)
+  end
+
 end
