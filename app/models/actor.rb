a50667d1144d00ace209586d161c567e994e7506
Misashi <joel.yang.xl@gmail.com>
2016-03-28 13:54:02 +0800

    actor-movie-models

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,11 @@
 class Actor < ActiveRecord::Base
+  has_and_belongs_to_many :movies
+
+
+  attr_reader :year_of_birth
+  attr_accessor :age
+
+  def age
+    Time.now.year - self.year_of_birth.year
+  end
 end
